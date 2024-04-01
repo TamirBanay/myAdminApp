8d6f20772e59c19c371b3e1d65db7064de42b23a
Tamir Banay <banay9329@gmail.com>
2024-04-01 14:28:38 +0300

    fix syntax

--- my-admin-app/src/pages/Tests/Tests.js
+++ my-admin-app/src/pages/Tests/Tests.js
@@ -156,7 +156,7 @@ function Tests() {
                   pingModulesWithMacAddress(module.macAddress, "Update");
                 }}
               >
-                UIpdate Firmware 
+                Update Firmware 
               </button>
               <button
                 className="button"
